28a01980dbceb8f0040e97cc08a0dfdce0b31ba7
Vignesh <vignesh.aln@gmail.com>
2021-03-25 11:39:40 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const https = require('https');
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+// const options = {
+//   key: fs.readFileSync('../../../Documents/Certs/localhost/privkey.pem'),
+//   cert: fs.readFileSync('../../../Documents/Certs/localhost/cert.pem')
+// };
+
+app.use(express.json());
+app.use(express.static("express"));
+
+// default URL for website
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
+app.get('/websterBank/websterBankCallback', function(req,res){
+res.sendFile(path.join(__dirname+'/express/websterBankCallback.html'));
+//__dirname : It will resolve to your project folder.
+});
+const server = https.createServer({
+  key: fs.readFileSync('../../../Documents/Certs/localhost/key.pem'),
+  cert: fs.readFileSync('../../../Documents/Certs/localhost/cert.pem')
+},app);
+const port = 443;
+server.listen(port);
+console.debug('Server listening on port ' + port);
